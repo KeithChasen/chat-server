418eae5e4fa7eb8fba470843ec7fb4a91942c06c
keithchasen <keithchasen89@gmail.com>
2024-01-20 22:50:05 +0100

    mongo set up

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 9999;
+const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
     console.log(`app running on port: ${port}`)
+});
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+.then(() => console.log('Mongo connected'))
+.catch(error => console.error("Mongo error: ", error.message));
+
