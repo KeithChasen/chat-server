31296c10b457ee482f208e16f39d9e3eb701379c
keithchasen <keithchasen89@gmail.com>
2024-01-21 21:03:11 +0100

    login user

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -48,4 +48,29 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        let user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json("Invalid email or password");
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            return res.status(400).json("Invalid email or password");
+        }
+
+        const token = createToken(user._id);
+    
+        res.status(200).json({_id: user._id, name: user.name, email, token });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+module.exports = { registerUser, loginUser };
